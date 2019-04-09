5a36717b36d7bfef16baee86a667636c118fdc2b
user-penguin <kobzev96@gmail.com>
2019-04-10 06:07:40 +0700

    Написаны step definitions для получения правильного ответа

--- src/main/java/GameKernelTest.java
+++ src/main/java/GameKernelTest.java
@@ -1,4 +1,5 @@
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -7,6 +8,8 @@ import org.junit.Assert;
 public class GameKernelTest {
     private static GameKernel gameKernel;
     private String resultAfterCall;
+    private int numberOfQuestion;
+    private String trueResult;
 
     @Given("^I have an object of GameKernel Class$")
     public void iHaveAnObjectOfGameKernelClass() {
@@ -26,5 +29,20 @@ public class GameKernelTest {
     @When("^I call this method again$")
     public void iCallThisMethodAgain() {
         resultAfterCall = gameKernel.getNextQuestion();
+    }
+
+    @When("^I have input number of question (\\d+)$")
+    public void iHaveInputNumberOfQuestion(int numberOfQuestion) {
+        this.numberOfQuestion = numberOfQuestion;
+    }
+
+    @And("^I have call the method that should return true answer$")
+    public void iHaveCallTheMethodThatShouldReturnTrueAnswer() {
+        trueResult = gameKernel.getTrueAnswer(numberOfQuestion);
+    }
+
+    @Then("^I get the answer \"([^\"]*)\"$")
+    public void iGetTheAnswer(String result) {
+        Assert.assertEquals(trueResult, result);
     }
 }
