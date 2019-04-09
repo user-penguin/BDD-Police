54d32d249d61f7ca9641522ccae588fab33ac103
user-penguin <kobzev96@gmail.com>
2019-04-09 15:05:39 +0700

    refactored step definitions for reading test

--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -1,10 +1,22 @@
+import java.io.*;
+
 public class ReadFile {
-    // @TODO реализовать чтение
     public String readFromFile(String operand) {
-        if (operand.equals("file.txt")) {
-            return "Привет";
-        } else {
-            return "Привет\nПока";
+        String pathToFile = ReadFile.class.getResource(operand).getPath();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(pathToFile);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String result = bufferedReader.readLine();
+            String unit = "";
+            while((unit = bufferedReader.readLine()) != null) {
+                result += "\n" + unit;
+            }
+            return result;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return "";
     }
 }
